36dff4d039b846c387368d0d38eaa3ea980e4fc1
unknown <cedicray20@gmail.com>
2024-12-17 04:07:25 +0000

    First commit

---
+++ qr_code.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img = qr.make("https://www.linkedin.com/in/aled-ebenezer-baafi-junior-636b71267?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BAB9HUnXZQdW%2FGyCXHHfLUQ%3D%3D")
+img.save("Aled Ebenezer Baafi Junior_linkdIn.png")
